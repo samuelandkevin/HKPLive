8b93834c5323fa47845466f2b2c4546667a1036e
kun <446570758@qq.com>
2019-03-03 18:08:02 +0800

    commit

---
+++ 高仿映客/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  高仿映客
+//
+//  Created by JIAAIR on 16/6/29.
+//  Copyright © 2016年 JIAAIR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
